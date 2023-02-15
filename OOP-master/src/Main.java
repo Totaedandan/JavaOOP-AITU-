4b54e14ebb47fbc73f7ff70b1469ea1482949d55
Tolebi Baitassov <113658550+Totaedandan@users.noreply.github.com>
2023-02-15 12:07:11 +0600

    Add files via upload

---
+++ OOP-master/src/Main.java
@@ -0,0 +1,9 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        ManageOfRequest manageOfRequest = new ManageOfRequest();
+        manageOfRequest.Requests();
+    }
+}
+
